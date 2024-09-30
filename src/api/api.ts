9be479beb7b52377293be51a23e9c991ba70fa05
Leandro Simões <Leandrosimoes42@gmail.com>
2024-09-30 03:54:29 -0300

    fix: enviroment

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const createApiInstance = (): AxiosInstance => {
   const config: AxiosRequestConfig = {
-    baseURL: "/api/challenge",
+    baseURL: "https://cors-anywhere.herokuapp.com/https://cdn-dev.preoday.com/challenge",
     timeout: 60 * 1000
   };
 
