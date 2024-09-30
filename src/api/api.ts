06ccfa87071cf5f8ca7ed8ffe4e7db4adbd5d87c
Leandro Simões <Leandrosimoes42@gmail.com>
2024-09-30 04:49:10 -0300

    fix: enviroment

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const createApiInstance = (): AxiosInstance => {
   const config: AxiosRequestConfig = {
-    baseURL: "/api/",
+    baseURL: "/api",
     timeout: 60 * 1000
   };
 
